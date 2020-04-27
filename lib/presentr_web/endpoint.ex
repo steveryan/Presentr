e6d1f7e1beafdc2a04b1d158d3d67e06c9504bcf
steveryan <steveryan6989@gmail.com>
2020-04-26 22:15:43 -0400

    getting ready for heroku

--- lib/presentr_web/endpoint.ex
+++ lib/presentr_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule PresentrWeb.Endpoint do
   ]
 
   socket "/socket", PresentrWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
